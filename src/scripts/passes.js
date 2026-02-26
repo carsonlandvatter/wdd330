3b269d44ba470ef61ef64193725462ef8eed781a
carsonlandvatter <landvattercarson@gmail.com>
2026-02-26 12:18:09 -0700

    added succes messages

--- src/scripts/passes.js
+++ src/scripts/passes.js
@@ -1,33 +1,40 @@
 const addSeasonPass = document.querySelector('#addSeasonPass');
 const addDayPass = document.querySelector('#addDayPass');
-const addNightPass = document.querySelector('#addNightPass')
+const addNightPass = document.querySelector('#addNightPass');
 const cart = document.getElementById('cart');
+const addSeasonMsg = document.getElementById('add-season-msg');
+const addDayMsg = document.getElementById('add-day-msg');
+const addNightMsg = document.getElementById('add-night-msg'); 
+const purchaseBtn = document.getElementById('purchase');
+const purchaseMsg = document.getElementById('purchase-msg');
 
 export default function addToCart() {
     if (addDayPass, addSeasonPass) {
     addSeasonPass.addEventListener('click', () => {
         localStorage.setItem("seasonPass", "Season Pass - $1000");
+        addSeasonMsg.textContent = "Item successfully added to cart."
     })
 
     addDayPass.addEventListener('click', () => {
-        localStorage.setItem("dayPass", "Day Pass - $80")
-
+        localStorage.setItem("dayPass", "Day Pass - $80");
+        addDayMsg.textContent = "Item successfully added to cart."
     })
 
     addNightPass.addEventListener('click', () => {
-        localStorage.setItem("nightPass", "Night pass - $60")
+        localStorage.setItem("nightPass", "Night pass - $60");
+        addNightMsg.textContent = "Item successfully added to cart."
     })
     }
+
     if (cart) {
     const seasonPass = localStorage.getItem("seasonPass");
     const dayPass = localStorage.getItem("dayPass");
-    const nightPass = localStorage.getItem("nightPass")
-    const totalPasses = []
+    const nightPass = localStorage.getItem("nightPass");
+    let totalPasses = []
 
     if (dayPass) totalPasses.push(dayPass);
     if (nightPass) totalPasses.push(nightPass);
     if (seasonPass) totalPasses.push(seasonPass);
-    console.log(totalPasses);
 
     totalPasses.forEach(pass => {
         const p = document.createElement('p');
@@ -39,7 +46,15 @@ export default function addToCart() {
 
     emptyCart.addEventListener('click', () => {
         localStorage.clear();
-        cart.innerHTML = '';
+        totalPasses = [];
+        cart.innerHTML = "";
+        purchaseMsg.textContent = "";
     })
+
+    purchaseBtn.addEventListener('click', () => {
+        if (totalPasses.length > 0) {
+            purchaseMsg.textContent = "Purchase successful!";
+        }
+    });
     }
 }
